af18f3f7982d51fa80fbeb66bf1856354b282e29
Nick Brennan <nick_p_b@yahoo.com>
2015-09-04 14:09:01 -0700

    finished basic user model and added auth statics and method. created seed.js and added it to .gitignore

--- models/user.js
+++ models/user.js
@@ -1,6 +1,44 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt');
 
 var userSchema = new Schema({
-	
+	email: {type: String, required: true, unique: true},
+	passwordDigest: {type: String, required: true},
+	createdAt: {type: Date, default: Date.now()},
+	username: String,
+	imageURL: String
 });
+
+UserSchema.statics.createSecure = function(email, password, cb){
+	var _this = this;
+	bycrypt.genSalt(function(err, salt)){
+		bcrypt.hash(password, salt, function(err, hash){
+			var user = {
+				email: email,
+				passwordDigest: hash
+			};
+			_this.create(user, cb)
+		});
+	});
+};
+
+UserSchema.statics.authenticate = function(emailOrUsername, password, cb){
+	this.findOne({$or[{email: emailOrUsername}, {username: emailOrUsername}]}, function(err, user){
+		if(user === null){
+			cb("Sorry, who's that?", null);
+		}else if(user.checkPassword(password)){
+			cb(null, user);
+		}else{
+			cb("Sorry, wrong password", user);
+		}
+	});
+};
+
+UserSchema.methods.checkPassword = function(password){
+	return bcrypt.compareSync(password, this.passwordDigest);
+}
+
+var User = mongoose.model("User", UserSchema);
+
+module.exports = User;
