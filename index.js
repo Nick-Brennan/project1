3d714912e4d7efd70c121ab59805dea6be741084
Nick Brennan <nick_p_b@yahoo.com>
2015-09-05 17:29:38 -0700

    added express-session dependency, extended req w/ session methods, roughed out login.html and dummy chat.html

--- index.js
+++ index.js
@@ -2,24 +2,77 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var path = require('path');
+var views = path.join(process.cwd(), "views"); 
 var db = require('./models');
 
+app.use("/static", express.static("public"));
+app.use("/vendor", express.static("bower_components"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(
+	session({
+		secret: 'secret-key',
+		resave: false,
+		saveUninitialized: true
+	})
+);
+app.use(function(req, res, next){
+	req.login = function(user){
+		req.session.userId = user._id;
+	};
 
-app.get('/', function(req, res){
-	res.send("Work in progress");
+	req.currentUser = function(cb){
+		db.User.findOne({_id: req.session.userId},
+			function(err, user){
+				req.user = user;
+				cb(null, user);
+			});
+	};
+
+	req.logout = function(){
+		req.session.userId = null;
+		req.user = null;
+	};
+
+	next();
+});
+
+///***Routes***///////////////////////////////////
+app.get(['/', '/login'], function(req, res){
+	res.sendFile(views + "/login.html");
 });
 
 app.post(['/api/users', '/signup'], function(req, res){
 	var user = req.body.user;
 	var email = user.email;
 	var password = user.password;
-	db.User.createSecure(email, password, function(err, user){
-		res.send(user + "was created and registered");
+	db.User.createSecure(email, password, function(err, newUser){
+		if(err){
+			console.log(err)
+		}else if(user === null){
+			res.redirect('/');
+		}else{
+			req.login(newUser);
+			res.redirect('/chat');
+		}
 	});
 });
 
-
+app.post(['/api/sessions', '/login'], function(req, res){
+	var user = req.body.user;
+	var email = user.email;
+	var password = user. password;
+	db.User.authenticate(email, password, function(err, user){
+		if(err){
+			console.log(err)
+		}else if(user === null){
+			res.redirect('/');
+		}else{
+			res.sendFile(views + "/chat.html");
+		}
+	});
+});
 
 http.listen(3000, function(){
 	console.log("Twitter Lounge is listening on port 3000");
