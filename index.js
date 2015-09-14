8784a1176bfb816ae7d0b386bc5d5333057f05d2
Francis Serpa <fcserpa@gmail.com>
2015-09-14 10:00:28 -0700

    fixed console.log issue

--- index.js
+++ index.js
@@ -202,7 +202,7 @@ stream.on('tweet', function (tweet) {
 ///***Server Setup***//////////////////////////////////////////////
 
 http.listen(process.env.PORT || 3000, function(){
-	console.log("Twitter Lounge is listening on port " + process.env.PORT || 3000);
+	console.log("Twitter Lounge is listening on port " + (process.env.PORT || 3000));
 });
 
 
